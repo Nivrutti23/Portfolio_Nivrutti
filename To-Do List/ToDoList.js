387876fd52108698904963a35bdadfd0f64ce245
Nivrutti Nadekar <137288999+Nivrutti23@users.noreply.github.com>
2023-11-01 01:08:49 +0530

    Add files via upload

---
+++ To-Do List/ToDoList.js
@@ -0,0 +1,10 @@
+const taskEntry = document.getElementById("taskEntry");
+const taskList = document.getElementById("task-list");
+
+function addTask() {
+  if (taskEntry.value == "") {
+    alert("Enter a Task");
+  } else {
+    let li = document.crEATE;
+  }
+}
